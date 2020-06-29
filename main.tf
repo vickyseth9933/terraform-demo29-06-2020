7a5f9397d0d390f5df28b565c9453f484bd5be90
vickky <vickyseth9933@gmail.com>
2020-06-29 10:12:22 +0530

    intiail file upload

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  version = "~> 2.0"
+  region = var.region
+  access_key = ""
+  secret_key = ""
+}
+
+data "aws_availability_zones" "azs" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.44.0"
+  name = "terraform-vpc-demo"
+  cidr = var.vpc_cidr_range
+
+  azs = slice(data.aws_availability_zones.azs.names, 0,2)
+  public_subnets = var.public-subnets
+
+  #Database server
+  database_subnets = var.database_subnet
+  database_subnet_tags = {
+    subnet_type = "val-terr"
+  }
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+
+
+}
+
